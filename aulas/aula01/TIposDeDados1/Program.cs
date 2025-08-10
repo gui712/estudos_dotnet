a2c1f17a551efb42bfb1e5ce52347bcef083f778
Guilherme Costa <guilhermecs_30@yahoo.com.br>
2025-08-10 01:19:17 -0300

    numeric types

---
+++ aulas/aula01/TIposDeDados1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Declarando variaveis de tipos primitivos");
+
+// declarando variaveis
+
+byte valor1 = 255; // 0 a 255
+sbyte valor2 = -128; // -128 a 127
+int valor3 = -3547894;
+uint valor4 = 3456879;
+long valor5 = -3456789876543; // -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807
+
+Console.WriteLine($"Valor 1: {valor1}");
+Console.WriteLine($"Valor 2: {valor2}");
+Console.WriteLine($"Valor 3: {valor3}");
+Console.WriteLine($"Valor 4: {valor4}");
+Console.WriteLine($"Valor 5: {valor5}");
---
+++ aulas/aula02/aula2/TIposDeDados2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Atribuindo valores a variáveis");
+Console.WriteLine("====================================");
+
+double n1 = 1.23;
+float n2 = 4.567f;
+decimal n3 = 8.90m;
+
+Console.WriteLine(n1);
+Console.WriteLine(n2);
+Console.WriteLine(n3);
